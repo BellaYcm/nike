80c3f161f63f82739afd9e04cfb35d9249ab1ded
sunkejl <sunkejl@live.com>
2015-03-05 21:22:36 +0800

    refs 0305

---
+++ config/mail.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunke_sk
+ * Date: 2015/3/5
+ * Time: 20:37
+ */
+return array(
+
+    'host' => 'smtp.sina.com',
+
+    'username' => 'sunkejl@sina.com',
+
+    'password' => 'ycm@2012!',
+
+    'secure' => ''
+
+);
